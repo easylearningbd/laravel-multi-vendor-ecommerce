fa86da73d022a43a9e14e91095a3a5b7032ad171
easylearningbd <elebdsoft@gmail.com>
2022-07-25 03:53:56 +0600

    36 User Profile¦ Password Change Part 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,6 +34,12 @@
     <li class="nav-item">
         <a class="nav-link" id="account-detail-tab" data-bs-toggle="tab" href="#account-detail" role="tab" aria-controls="account-detail" aria-selected="true"><i class="fi-rs-user mr-10"></i>Account details</a>
     </li>
+
+      <li class="nav-item">
+        <a class="nav-link" id="change-password-tab" data-bs-toggle="tab" href="#change-password" role="tab" aria-controls="change-password" aria-selected="true"><i class="fi-rs-user mr-10"></i>Change Password</a>
+    </li>
+
+
     <li class="nav-item">
         <a class="nav-link" href="{{ route('user.logout') }}"><i class="fi-rs-sign-out mr-10"></i>Logout</a>
     </li>
@@ -224,6 +230,87 @@
         </div>
     </div>
                                     </div>
+
+
+
+
+
+ <!-- /// Change Password  -->
+
+<div class="tab-pane fade" id="change-password" role="tabpanel" aria-labelledby="change-password-tab">
+    <div class="card">
+        <div class="card-header">
+            <h5>Change Password</h5>
+        </div>
+        <div class="card-body">
+            
+            
+
+    <form method="post" action="{{ route('user.profile.store') }}" > 
+            @csrf
+
+         @if (session('status'))
+         <div class="alert alert-success" role="alert">
+                {{session('status')}}
+         </div>
+         @elseif(session('error'))
+         <div class="alert alert-danger" role="alert">
+            {{session('error')}}
+         </div>
+         @endif
+
+
+<div class="row">
+    
+    <div class="form-group col-md-12">
+        <label>Old Password <span class="required">*</span></label>
+        <input  class="form-control @error('old_password') is-invalid @enderror"  name="old_password" type="password" id="current_password"    placeholder="Old Password"  />
+
+        @error('old_password')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+
+      <div class="form-group col-md-12">
+        <label>New Password <span class="required">*</span></label>
+        <input  class="form-control @error('new_password') is-invalid @enderror"  name="new_password" type="password" id="new_password"   placeholder="New Password"  />
+        
+        @error('new_password')
+        <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </div>
+
+
+      <div class="form-group col-md-12">
+        <label>Confirm New Password <span class="required">*</span></label>
+        <input  class="form-control"  name="new_password_confirmation" type="password" id="new_password_confirmation"  placeholder="Confirm New Password"  /> 
+       
+    </div>
+    
+   
+    
+    <div class="col-md-12">
+        <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit" value="Submit">Save Change</button>
+    </div>
+</div>
+            </form>
+        </div>
+    </div>
+                                    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                                 </div>
                             </div>
                         </div>

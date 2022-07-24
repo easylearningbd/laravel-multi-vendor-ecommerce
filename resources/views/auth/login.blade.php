aa8fa733b25a50ff13430f74c2cc4fc856754cb0
easylearningbd <elebdsoft@gmail.com>
2022-07-25 01:53:34 +0600

    29 Frontend Template Login Page Setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,126 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
+    <meta charset="utf-8" />
+    <title>Easy Shop Online Store </title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/imgs/theme/favicon.svg') }}" />
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/main.css?v=5.3') }}" />
+</head>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<body>
+   
+     @include('frontend.body.header')
 
-        <form method="POST" action="{{ route('login') }}">
+   <!-- End Header  --> 
+ 
+    
+
+    <main class="main pages">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
+                    <span></span>  My Account
+                </div>
+            </div>
+        </div>
+        <div class="page-content pt-150 pb-150">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                        <div class="row">
+                            <div class="col-lg-6 pr-30 d-none d-lg-block">
+                                <img class="border-radius-15" src="{{ asset('frontend/assets/imgs/page/login-1.png') }}" alt="" />
+                            </div>
+<div class="col-lg-6 col-md-8">
+    <div class="login_wrap widget-taber-content background-white">
+        <div class="padding_eight_all bg-white">
+            <div class="heading_s1">
+                <h1 class="mb-5">Login</h1>
+                <p class="mb-30">Don't have an account? <a href="page-register.html">Create here</a></p>
+            </div>
+
+
+           <form method="POST" action="{{ route('login') }}">
             @csrf
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <div class="form-group">
+                    <input type="email" id="email"  required="" name="email" placeholder="Username or Email *" />
+                </div>
+                <div class="form-group">
+                    <input required="" id="password" type="password" name="password" placeholder="Your password *" />
+                </div>
+                 
+                <div class="login_footer form-group mb-50">
+                    <div class="chek-form">
+                        <div class="custome-checkbox">
+                            <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox1" value="" />
+                            <label class="form-check-label" for="exampleCheckbox1"><span>Remember me</span></label>
+                        </div>
+                    </div>
+                    <a class="text-muted" href="{{ route('password.request') }}">Forgot password?</a>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-heading btn-block hover-up" name="login">Log in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+    </main>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+      @include('frontend.body.footer')
+
+
+    <!-- Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="text-center">
+                    <img src="{{ asset('frontend/assets/imgs/theme/loading.gif') }}" alt="" />
+                </div>
             </div>
+        </div>
+    </div>
+    <!-- Vendor JS-->
+   <script src="{{ asset('frontend/assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/slick.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.syotimer.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/waypoints.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/wow.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/magnific-popup.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/select2.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/counterup.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/images-loaded.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/isotope.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/scrollup.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.vticker-min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.theia.sticky.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.elevatezoom.js') }}"></script>
+    <!-- Template  JS -->
+    <script src="{{ asset('frontend/assets/js/main.js?v=5.3') }}"></script>
+    <script src="{{ asset('frontend/assets/js/shop.js?v=5.3') }}"></script>
+</body>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>

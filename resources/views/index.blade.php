9403db9fdab010fa075d0177d8c965bdd70ed51c
easylearningbd <elebdsoft@gmail.com>
2022-07-25 03:22:24 +0600

    33 User Profile Design Part 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,8 @@
 @extends('dashboard') 
 @section('user')
- 
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+
   <div class="page-header breadcrumb-wrap">
             <div class="container">
                 <div class="breadcrumb">
@@ -158,47 +160,59 @@
         </div>
     </div>
 </div>
+
+
+
+
+
+
 <div class="tab-pane fade" id="account-detail" role="tabpanel" aria-labelledby="account-detail-tab">
     <div class="card">
         <div class="card-header">
             <h5>Account Details</h5>
         </div>
         <div class="card-body">
-            <p>Already have an account? <a href="page-login.html">Log in instead!</a></p>
+            
+            
+
             <form method="post" name="enq">
-                <div class="row">
-                    <div class="form-group col-md-6">
-                        <label>First Name <span class="required">*</span></label>
-                        <input required="" class="form-control" name="name" type="text" />
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label>Last Name <span class="required">*</span></label>
-                        <input required="" class="form-control" name="phone" />
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label>Display Name <span class="required">*</span></label>
-                        <input required="" class="form-control" name="dname" type="text" />
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label>Email Address <span class="required">*</span></label>
-                        <input required="" class="form-control" name="email" type="email" />
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label>Current Password <span class="required">*</span></label>
-                        <input required="" class="form-control" name="password" type="password" />
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label>New Password <span class="required">*</span></label>
-                        <input required="" class="form-control" name="npassword" type="password" />
-                    </div>
-                    <div class="form-group col-md-12">
-                        <label>Confirm Password <span class="required">*</span></label>
-                        <input required="" class="form-control" name="cpassword" type="password" />
-                    </div>
-                    <div class="col-md-12">
-                        <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit" value="Submit">Save Change</button>
-                    </div>
-                </div>
+<div class="row">
+    <div class="form-group col-md-6">
+        <label>User Name <span class="required">*</span></label>
+        <input required="" class="form-control" name="username" type="text" value="{{ $userData->username }}" />
+    </div>
+    <div class="form-group col-md-6">
+        <label>Full Name <span class="required">*</span></label>
+        <input required="" class="form-control" name="name" value="{{ $userData->name }}" />
+    </div>
+    <div class="form-group col-md-12">
+        <label>Email <span class="required">*</span></label>
+        <input required="" class="form-control" name="email" type="text" value="{{ $userData->email }}" />
+    </div>
+    <div class="form-group col-md-12">
+        <label>Phone <span class="required">*</span></label>
+        <input required="" class="form-control" name="phone" type="text" value="{{ $userData->phone }}" />
+    </div>
+    <div class="form-group col-md-12">
+        <label>Address <span class="required">*</span></label>
+        <input required="" class="form-control" name="address" type="text" value="{{ $userData->address }}" />
+    </div>
+    <div class="form-group col-md-12">
+        <label>User Photo <span class="required">*</span></label>
+        <input class="form-control" name="photo" type="file"  id="image" />
+    </div>
+
+      <div class="form-group col-md-12">
+        <label>  <span class="required">*</span></label>
+        <img id="showImage" src="{{ (!empty($userData->photo)) ? url('upload/user_images/'.$userData->photo):url('upload/no_image.jpg') }}" alt="User" class="rounded-circle p-1 bg-primary" width="110">
+    </div>
+
+
+    
+    <div class="col-md-12">
+        <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit" value="Submit">Save Change</button>
+    </div>
+</div>
             </form>
         </div>
     </div>
@@ -210,5 +224,21 @@
                 </div>
             </div>
         </div>
+
+        <script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+
+
+</script>
+
+
 
 @endsection

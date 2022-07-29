ac983cae6defe343f39c7b07e9d64427ff09ed70
easylearningbd <elebdsoft@gmail.com>
2022-07-30 02:51:09 +0600

    62 Add Vendor Register Page Setup Part 8

--- resources/views/vendor/body/sidebar.blade.php
+++ resources/views/vendor/body/sidebar.blade.php
@@ -1,3 +1,12 @@
+
+@php
+	$id = Auth::user()->id;
+	$verdorId = App\Models\User::find($id);
+	$status = $verdorId->status; 
+@endphp
+ 
+
+
 <div class="sidebar-wrapper" data-simplebar="true">
 			<div class="sidebar-header">
 				<div>
@@ -21,76 +30,44 @@
 				</li>
 
 
+           @if($status === 'active')
+
 				<li>
 					<a href="javascript:;" class="has-arrow">
 						<div class="parent-icon"><i class='bx bx-home-circle'></i>
 						</div>
-						<div class="menu-title">Brand</div>
+						<div class="menu-title">Product Manage </div>
 					</a>
 					<ul>
-						<li> <a href="index.html"><i class="bx bx-right-arrow-alt"></i>Default</a>
+						<li> <a href="index.html"><i class="bx bx-right-arrow-alt"></i>All Product</a>
 						</li>
-						<li> <a href="dashboard-eCommerce.html"><i class="bx bx-right-arrow-alt"></i>eCommerce</a>
+						<li> <a href="dashboard-eCommerce.html"><i class="bx bx-right-arrow-alt"></i>Add Product</a>
 						</li>
-						<li> <a href="dashboard-analytics.html"><i class="bx bx-right-arrow-alt"></i>Analytics</a>
-						</li>
-						<li> <a href="dashboard-digital-marketing.html"><i class="bx bx-right-arrow-alt"></i>Digital Marketing</a>
-						</li>
-						<li> <a href="dashboard-human-resources.html"><i class="bx bx-right-arrow-alt"></i>Human Resources</a>
-						</li>
+						 
 					</ul>
 				</li>
 				<li>
 					<a href="javascript:;" class="has-arrow">
 						<div class="parent-icon"><i class="bx bx-category"></i>
 						</div>
-						<div class="menu-title">Application</div>
+						<div class="menu-title">All Order</div>
 					</a>
 					<ul>
 						<li> <a href="app-emailbox.html"><i class="bx bx-right-arrow-alt"></i>Email</a>
 						</li>
 						<li> <a href="app-chat-box.html"><i class="bx bx-right-arrow-alt"></i>Chat Box</a>
 						</li>
-						<li> <a href="app-file-manager.html"><i class="bx bx-right-arrow-alt"></i>File Manager</a>
-						</li>
-						<li> <a href="app-contact-list.html"><i class="bx bx-right-arrow-alt"></i>Contatcs</a>
-						</li>
-						<li> <a href="app-to-do.html"><i class="bx bx-right-arrow-alt"></i>Todo List</a>
-						</li>
-						<li> <a href="app-invoice.html"><i class="bx bx-right-arrow-alt"></i>Invoice</a>
-						</li>
-						<li> <a href="app-fullcalender.html"><i class="bx bx-right-arrow-alt"></i>Calendar</a>
-						</li>
+						 
 					</ul>
 				</li>
-				 
-			
-				 
-				 
-			  
 
+				
+	
+	@else
 
-
-				 
+	@endif			 
 			 
-			   
-				<li class="menu-label">Charts & Maps</li>
-				<li>
-					<a class="has-arrow" href="javascript:;">
-						<div class="parent-icon"><i class="bx bx-line-chart"></i>
-						</div>
-						<div class="menu-title">Charts</div>
-					</a>
-					<ul>
-						<li> <a href="charts-apex-chart.html"><i class="bx bx-right-arrow-alt"></i>Apex</a>
-						</li>
-						<li> <a href="charts-chartjs.html"><i class="bx bx-right-arrow-alt"></i>Chartjs</a>
-						</li>
-						<li> <a href="charts-highcharts.html"><i class="bx bx-right-arrow-alt"></i>Highcharts</a>
-						</li>
-					</ul>
-				</li>
-				 
+ 
 		 
 			  
 				<li>
--- resources/views/vendor/index.blade.php
+++ resources/views/vendor/index.blade.php
@@ -1,7 +1,21 @@
 @extends('vendor.vendor_dashboard')
 @section('vendor')
 
+@php
+	$id = Auth::user()->id;
+	$verdorId = App\Models\User::find($id);
+	$status = $verdorId->status; 
+@endphp
+ 
 <div class="page-content">
+
+
+	@if($status === 'active')
+	<h4>Vendor Account is <span class="text-success">Active</span> </h4>
+	@else
+	<h4>Vendor Account is <span class="text-danger">InActive</span> </h4>
+	<p class="text-danger"><b> Plz wait admin will check and approve your account</b></p>
+	@endif
 
 					<div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
 						<div class="col">

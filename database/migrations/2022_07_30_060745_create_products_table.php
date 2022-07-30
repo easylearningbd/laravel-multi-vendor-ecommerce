a504cce6dc30495753a5ab1f3377a917024206ef
easylearningbd <elebdsoft@gmail.com>
2022-07-30 12:22:04 +0600

    63 Add Product Database and Page Design Part 1

---
+++ database/migrations/2022_07_30_060745_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('brand_id');
+            $table->integer('category_id');
+            $table->integer('subcategory_id');
+            $table->string('product_name');
+            $table->string('product_slug');
+            $table->string('product_code');
+            $table->string('product_qty');
+            $table->string('product_tags')->nullable();
+            $table->string('product_size')->nullable();
+            $table->string('product_color')->nullable();
+            $table->string('selling_price');
+            $table->string('discount_price')->nullable();
+            $table->text('short_descp');
+            $table->text('long_descp');
+            $table->string('product_thambnail');
+            $table->string('vendor_id')->nullable();
+            $table->integer('hot_deals')->nullable();
+            $table->integer('featured')->nullable();
+            $table->integer('special_offer')->nullable();
+            $table->integer('special_deals')->nullable();
+            $table->integer('status')->default(0); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -67,6 +67,22 @@
 				</li>
 
 
+				<li>
+					<a href="javascript:;" class="has-arrow">
+						<div class="parent-icon"><i class="bx bx-category"></i>
+						</div>
+						<div class="menu-title">Product Manage</div>
+					</a>
+					<ul>
+						<li> <a href="{{ route('all.subcategory') }}"><i class="bx bx-right-arrow-alt"></i>All Product</a>
+						</li>
+						<li> <a href="{{ route('add.subcategory') }}"><i class="bx bx-right-arrow-alt"></i>Add Product</a>
+						</li>
+						 
+					</ul>
+				</li>
+
+
 				<li class="menu-label">UI Elements</li>
 			
 				<li>

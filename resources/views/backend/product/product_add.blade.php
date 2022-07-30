dfd02dbf47a3ab8cb49e1559001af4a17fc5cf14
easylearningbd <elebdsoft@gmail.com>
2022-07-30 13:46:47 +0600

    67 Show Image Using Javascript

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.admin_dashboard')
 @section('admin')
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <div class="page-content">
 
 				<!--breadcrumb-->
@@ -65,14 +65,19 @@
 
   <div class="mb-3">
 				<label for="inputProductTitle" class="form-label">Main Thambnail</label>
-				<input name="product_thambnail" class="form-control" type="file" id="formFile">
+				<input name="product_thambnail" class="form-control" type="file" id="formFile" onChange="mainThamUrl(this)" >
+
+				<img src="" id="mainThmb" />
 			  </div>
 
 
 
   <div class="mb-3">
 				<label for="inputProductTitle" class="form-label">Multiple Image</label>
-				<input class="form-control" name="multi_img[]" type="file" id="formFileMultiple" multiple="">
+				<input class="form-control" name="multi_img[]" type="file" id="multiImg" multiple="">
+
+			<div class="row" id="preview_img"></div>
+
 			  </div>
 
 
@@ -161,7 +166,7 @@
 	</div>
 
 
-	
+
 
 <div class="col-md-6">	
     <div class="form-check">
@@ -204,7 +209,48 @@
 
 
 
+<script type="text/javascript">
+	function mainThamUrl(input){
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function(e){
+				$('#mainThmb').attr('src',e.target.result).width(80).height(80);
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+</script>
 
+
+<script> 
+ 
+  $(document).ready(function(){
+   $('#multiImg').on('change', function(){ //on file input change
+      if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+      {
+          var data = $(this)[0].files; //this file data
+           
+          $.each(data, function(index, file){ //loop though each file
+              if(/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)){ //check supported file type
+                  var fRead = new FileReader(); //new filereader
+                  fRead.onload = (function(file){ //trigger function on successful read
+                  return function(e) {
+                      var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(100)
+                  .height(80); //create image element 
+                      $('#preview_img').append(img); //append image to output element
+                  };
+                  })(file);
+                  fRead.readAsDataURL(file); //URL representing the file's data.
+              }
+          });
+           
+      }else{
+          alert("Your browser doesn't support File API!"); //if File API is absent
+      }
+   });
+  });
+   
+  </script>
 
 
 @endsection

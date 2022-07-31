15e4c76e70693ecce9223dc7d1ca4c2ae5a90ada
easylearningbd <elebdsoft@gmail.com>
2022-07-31 11:37:17 +0600

    74 Manage Product Read All Product

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -33,6 +33,8 @@
 <!-- DataTable-->
 	 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
 	<title>Admin Dashboard</title>
 </head>
--- resources/views/backend/product/product_all.blade.php
+++ resources/views/backend/product/product_all.blade.php
@@ -47,12 +47,39 @@
 				<td>{{ $item->product_name }}</td>
 				<td>{{ $item->selling_price }}</td>
 				<td>{{ $item->product_qty }}</td>
-				<td>{{ $item->discount_price }}</td>
-				<td>{{ $item->status }}</td>
+
+				<td>
+					@if($item->discount_price == NULL)
+			<span class="badge rounded-pill bg-info">No Discount</span>
+			@else
+			@php
+			$amount = $item->selling_price - $item->discount_price;
+			$discount = ($amount/$item->selling_price) * 100;
+			@endphp
+		<span class="badge rounded-pill bg-danger"> {{ round($discount) }}%</span>
+			@endif
+					 </td>
+
+ 
+
+				<td> @if($item->status == 1)
+					<span class="badge rounded-pill bg-success">Active</span>
+					@else
+					<span class="badge rounded-pill bg-danger">InActive</span>
+					@endif
+				   </td>
 				
 				<td>
-<a href="{{ route('edit.category',$item->id) }}" class="btn btn-info">Edit</a>
-<a href="{{ route('delete.category',$item->id) }}" class="btn btn-danger" id="delete" >Delete</a>
+<a href="{{ route('edit.category',$item->id) }}" class="btn btn-info" title="Edit Data"> <i class="fa fa-pencil"></i> </a>
+<a href="{{ route('delete.category',$item->id) }}" class="btn btn-danger" id="delete" title="Delete Data" ><i class="fa fa-trash"></i></a>
+
+<a href="{{ route('edit.category',$item->id) }}" class="btn btn-warning" title="Details Page"> <i class="fa fa-eye"></i> </a>
+
+@if($item->status == 1)
+<a href="{{ route('edit.category',$item->id) }}" class="btn btn-primary" title="Inactive"> <i class="fa-solid fa-thumbs-down"></i> </a>
+@else
+<a href="{{ route('edit.category',$item->id) }}" class="btn btn-primary" title="Active"> <i class="fa-solid fa-thumbs-up"></i> </a>
+@endif
 
 				</td> 
 			</tr>

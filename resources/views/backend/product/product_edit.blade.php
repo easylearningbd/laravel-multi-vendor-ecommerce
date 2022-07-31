8f4818b85f39fdeed891afc286b68905de1dbfca
easylearningbd <elebdsoft@gmail.com>
2022-07-31 12:11:09 +0600

    76 Manage Product Edit Option Part 2

--- resources/views/backend/product/product_edit.blade.php
+++ resources/views/backend/product/product_edit.blade.php
@@ -102,7 +102,7 @@
 					<select name="brand_id" class="form-select" id="inputProductType">
 						<option></option>
 						@foreach($brands as $brand)
-						<option value="{{ $brand->id }}">{{ $brand->brand_name }}</option>
+ 	<option value="{{ $brand->id }}" {{ $brand->id == $products->brand_id ? 'selected' : '' }} >{{ $brand->brand_name }}</option>
 						 @endforeach
 					  </select>
 				  </div>
@@ -112,7 +112,7 @@
 					<select name="category_id" class="form-select" id="inputVendor">
 						<option></option>
 						@foreach($categories as $cat)
-						<option value="{{ $cat->id }}">{{ $cat->category_name }}</option>
+						<option value="{{ $cat->id }}" {{ $cat->id == $products->category_id ? 'selected' : '' }}>{{ $cat->category_name }}</option>
 						 @endforeach
 					  </select>
 				  </div>
@@ -122,7 +122,7 @@
 					<select name="subcategory_id" class="form-select" id="inputCollection">
 						<option></option>
 						@foreach($subcategory as $subcat)
-						<option value="{{ $subcat->id }}">{{ $subcat->subcategory_name }}</option>
+						<option value="{{ $subcat->id }}" {{ $subcat->id == $products->subcategory_id ? 'selected' : '' }}>{{ $subcat->subcategory_name }}</option>
 						 @endforeach
 						 
 					  </select>
@@ -134,7 +134,7 @@
 					<select name="vendor_id" class="form-select" id="inputCollection">
 						<option></option>
 					@foreach($activeVendor as $vendor)
-						<option value="{{ $vendor->id }}">{{ $vendor->name }}</option>
+						<option value="{{ $vendor->id }}" {{ $vendor->id == $products->vendor_id ? 'selected' : '' }}>{{ $vendor->name }}</option>
 						 @endforeach
 					  </select>
 				  </div>
@@ -146,14 +146,14 @@
 
 	 <div class="col-md-6">	
     <div class="form-check">
- <input class="form-check-input" name="hot_deals" type="checkbox" value="1" id="flexCheckDefault">
+ <input class="form-check-input" name="hot_deals" type="checkbox" value="1" id="flexCheckDefault" {{ $products->hot_deals == 1 ? 'checked' : '' }} >
 			<label class="form-check-label" for="flexCheckDefault"> Hot Deals</label>
 		</div>
 	</div>
 
 	<div class="col-md-6">	
     <div class="form-check">
-			<input class="form-check-input" name="featured" type="checkbox" value="1" id="flexCheckDefault">
+			<input class="form-check-input" name="featured" type="checkbox" value="1" id="flexCheckDefault" {{ $products->featured == 1 ? 'checked' : '' }}>
 			<label class="form-check-label" for="flexCheckDefault">Featured</label>
 		</div>
 	</div>
@@ -163,7 +163,7 @@
 
 <div class="col-md-6">	
     <div class="form-check">
-			<input class="form-check-input" name="special_offer" type="checkbox" value="1" id="flexCheckDefault">
+			<input class="form-check-input" name="special_offer" type="checkbox" value="1" id="flexCheckDefault" {{ $products->special_offer == 1 ? 'checked' : '' }}>
 			<label class="form-check-label" for="flexCheckDefault">Special Offer</label>
 		</div>
 	</div>
@@ -171,7 +171,7 @@
 
 	<div class="col-md-6">	
     <div class="form-check">
-			<input class="form-check-input" name="special_deals" type="checkbox" value="1" id="flexCheckDefault">
+			<input class="form-check-input" name="special_deals" type="checkbox" value="1" id="flexCheckDefault" {{ $products->special_deals == 1 ? 'checked' : '' }}>
 			<label class="form-check-label" for="flexCheckDefault">Special Deals</label>
 		</div>
 	</div>

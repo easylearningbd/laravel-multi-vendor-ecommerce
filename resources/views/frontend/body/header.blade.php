9f0eb4c6eb939b0b7044d43e024d6835b5352ce8
easylearningbd <elebdsoft@gmail.com>
2022-08-06 14:38:35 +0600

    100 Frontend Show Category Subcategory Part 2

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -276,157 +276,37 @@
                         </div>
                         <div class="main-menu main-menu-padding-1 main-menu-lh-2 d-none d-lg-block font-heading">
                             <nav>
-                                <ul>
-                                    
-                                    <li>
-                                        <a class="active" href="index.html">Home  </a>
-                                        
-                                    </li>
-                                    <li>
-                                        <a href="page-about.html">About</a>
-                                    </li>
-                                    <li>
-                                        <a href="shop-grid-right.html">Shop <i class="fi-rs-angle-down"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="shop-grid-right.html">Shop Grid – Right Sidebar</a></li>
-                                            <li><a href="shop-grid-left.html">Shop Grid – Left Sidebar</a></li>
-                                            <li><a href="shop-list-right.html">Shop List – Right Sidebar</a></li>
-                                            <li><a href="shop-list-left.html">Shop List – Left Sidebar</a></li>
-                                            <li><a href="shop-fullwidth.html">Shop - Wide</a></li>
-                                            <li>
-                                                <a href="#">Single Product <i class="fi-rs-angle-right"></i></a>
-                                                <ul class="level-menu">
-                                                    <li><a href="shop-product-right.html">Product – Right Sidebar</a></li>
-                                                    <li><a href="shop-product-left.html">Product – Left Sidebar</a></li>
-                                                    <li><a href="shop-product-full.html">Product – No sidebar</a></li>
-                                                    <li><a href="shop-product-vendor.html">Product – Vendor Info</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="shop-filter.html">Shop – Filter</a></li>
-                                            <li><a href="shop-wishlist.html">Shop – Wishlist</a></li>
-                                            <li><a href="shop-cart.html">Shop – Cart</a></li>
-                                            <li><a href="shop-checkout.html">Shop – Checkout</a></li>
-                                            <li><a href="shop-compare.html">Shop – Compare</a></li>
-                                            <li>
-                                                <a href="#">Shop Invoice<i class="fi-rs-angle-right"></i></a>
-                                                <ul class="level-menu">
-                                                    <li><a href="shop-invoice-1.html">Shop Invoice 1</a></li>
-                                                    <li><a href="shop-invoice-2.html">Shop Invoice 2</a></li>
-                                                    <li><a href="shop-invoice-3.html">Shop Invoice 3</a></li>
-                                                    <li><a href="shop-invoice-4.html">Shop Invoice 4</a></li>
-                                                    <li><a href="shop-invoice-5.html">Shop Invoice 5</a></li>
-                                                    <li><a href="shop-invoice-6.html">Shop Invoice 6</a></li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#">Vendors <i class="fi-rs-angle-down"></i></a>
-                                        <ul class="sub-menu">
-                                            <li><a href="vendors-grid.html">Vendors Grid</a></li>
-                                            <li><a href="vendors-list.html">Vendors List</a></li>
-                                            <li><a href="vendor-details-1.html">Vendor Details 01</a></li>
-                                            <li><a href="vendor-details-2.html">Vendor Details 02</a></li>
-                                            <li><a href="vendor-dashboard.html">Vendor Dashboard</a></li>
-                                            <li><a href="vendor-guide.html">Vendor Guide</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="position-static">
-                                        <a href="#">Mega menu <i class="fi-rs-angle-down"></i></a>
-                                        <ul class="mega-menu">
-                                            <li class="sub-mega-menu sub-mega-menu-width-22">
-                                                <a class="menu-title" href="#">Fruit & Vegetables</a>
-                                                <ul>
-                                                    <li><a href="shop-product-right.html">Meat & Poultry</a></li>
-                                                    <li><a href="shop-product-right.html">Fresh Vegetables</a></li>
-                                                    <li><a href="shop-product-right.html">Herbs & Seasonings</a></li>
-                                                    <li><a href="shop-product-right.html">Cuts & Sprouts</a></li>
-                                                    <li><a href="shop-product-right.html">Exotic Fruits & Veggies</a></li>
-                                                    <li><a href="shop-product-right.html">Packaged Produce</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="sub-mega-menu sub-mega-menu-width-22">
-                                                <a class="menu-title" href="#">Breakfast & Dairy</a>
-                                                <ul>
-                                                    <li><a href="shop-product-right.html">Milk & Flavoured Milk</a></li>
-                                                    <li><a href="shop-product-right.html">Butter and Margarine</a></li>
-                                                    <li><a href="shop-product-right.html">Eggs Substitutes</a></li>
-                                                    <li><a href="shop-product-right.html">Marmalades</a></li>
-                                                    <li><a href="shop-product-right.html">Sour Cream</a></li>
-                                                    <li><a href="shop-product-right.html">Cheese</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="sub-mega-menu sub-mega-menu-width-22">
-                                                <a class="menu-title" href="#">Meat & Seafood</a>
-                                                <ul>
-                                                    <li><a href="shop-product-right.html">Breakfast Sausage</a></li>
-                                                    <li><a href="shop-product-right.html">Dinner Sausage</a></li>
-                                                    <li><a href="shop-product-right.html">Chicken</a></li>
-                                                    <li><a href="shop-product-right.html">Sliced Deli Meat</a></li>
-                                                    <li><a href="shop-product-right.html">Wild Caught Fillets</a></li>
-                                                    <li><a href="shop-product-right.html">Crab and Shellfish</a></li>
-                                                </ul>
-                                            </li>
-                        <li class="sub-mega-menu sub-mega-menu-width-34">
-                            <div class="menu-banner-wrap">
-                                <a href="shop-product-right.html"><img src="{{ asset('frontend/assets/imgs/banner/banner-menu.png') }}" alt="Nest" /></a>
-                                <div class="menu-banner-content">
-                                    <h4>Hot deals</h4>
-                                    <h3>
-                                        Don't miss<br />
-                                        Trending
-                                    </h3>
-                                    <div class="menu-banner-price">
-                                        <span class="new-price text-success">Save to 50%</span>
-                                    </div>
-                                    <div class="menu-banner-btn">
-                                        <a href="shop-product-right.html">Shop now</a>
-                                    </div>
-                                </div>
-                                <div class="menu-banner-discount">
-                                    <h3>
-                                        <span>25%</span>
-                                        off
-                                    </h3>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="blog-category-grid.html">Blog <i class="fi-rs-angle-down"></i></a>
-                    <ul class="sub-menu">
-                        <li><a href="blog-category-grid.html">Blog Category Grid</a></li>
-                        <li><a href="blog-category-list.html">Blog Category List</a></li>
-                        <li><a href="blog-category-big.html">Blog Category Big</a></li>
-                        <li><a href="blog-category-fullwidth.html">Blog Category Wide</a></li>
+                    <ul>
+                        
                         <li>
-                            <a href="#">Single Post <i class="fi-rs-angle-right"></i></a>
-                            <ul class="level-menu level-menu-modify">
-                                <li><a href="blog-post-left.html">Left Sidebar</a></li>
-                                <li><a href="blog-post-right.html">Right Sidebar</a></li>
-                                <li><a href="blog-post-fullwidth.html">No Sidebar</a></li>
-                            </ul>
+                            <a class="active" href="index.html">Home  </a>
+                            
                         </li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Pages <i class="fi-rs-angle-down"></i></a>
-                    <ul class="sub-menu">
-                        <li><a href="page-about.html">About Us</a></li>
-                        <li><a href="page-contact.html">Contact</a></li>
-                        <li><a href="page-account.html">My Account</a></li>
-                        <li><a href="page-login.html">Login</a></li>
-                        <li><a href="page-register.html">Register</a></li>
-                        <li><a href="page-forgot-password.html">Forgot password</a></li>
-                        <li><a href="page-reset-password.html">Reset password</a></li>
-                        <li><a href="page-purchase-guide.html">Purchase Guide</a></li>
-                        <li><a href="page-privacy-policy.html">Privacy Policy</a></li>
-                        <li><a href="page-terms.html">Terms of Service</a></li>
-                        <li><a href="page-404.html">404 Page</a></li>
-                    </ul>
-                </li>
-                <li>
+    
+        @php
+
+    $categories = App\Models\Category::orderBy('category_name','ASC')->limit(6)->get();
+        @endphp
+
+       @foreach($categories as $category)    
+        <li>
+            <a href="#">{{ $category->category_name }} <i class="fi-rs-angle-down"></i></a>
+
+   @php 
+    $subcategories = App\Models\SubCategory::where('category_id',$category->id)->orderBy('subcategory_name','ASC')->get();
+        @endphp
+
+            <ul class="sub-menu">
+                @foreach($subcategories as $subcategory)   
+                <li><a href="vendors-grid.html">{{ $subcategory->subcategory_name }}</a></li>
+                @endforeach
+            </ul>
+        </li>
+        @endforeach
+
+
+                         
+               <li>
                     <a href="page-contact.html">Contact</a>
                 </li>
             </ul>

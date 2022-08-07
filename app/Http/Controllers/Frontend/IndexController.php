9af9452bfced60c7ba2e2bcf367473c947a334a7
easylearningbd <elebdsoft@gmail.com>
2022-08-08 02:03:44 +0600

    118 Special Deals Product Show

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -28,7 +28,11 @@ class IndexController extends Controller
 
         $special_offer = Product::where('special_offer',1)->orderBy('id','DESC')->limit(3)->get();
 
-        return view('frontend.index',compact('skip_category_0','skip_product_0','skip_category_2','skip_product_2','skip_category_7','skip_product_7','hot_deals','special_offer'));
+        $new = Product::where('status',1)->orderBy('id','DESC')->limit(3)->get();
+
+        $special_deals = Product::where('special_deals',1)->orderBy('id','DESC')->limit(3)->get();
+
+        return view('frontend.index',compact('skip_category_0','skip_product_0','skip_category_2','skip_product_2','skip_category_7','skip_product_7','hot_deals','special_offer','new','special_deals'));
 
     } // End Method 
 
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -473,131 +473,82 @@
                     <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 d-none d-lg-block wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
                         <h4 class="section-title style-1 mb-30 animated animated">Recently added</h4>
                         <div class="product-list-small animated animated">
-                            <article class="row align-items-center hover-up">
-                                <figure class="col-md-4 mb-0">
-                                    <a href="shop-product-right.html"><img src="{{ asset('frontend/assets/imgs/shop/thumbnail-7.jpg') }}" alt="" /></a>
-                                </figure>
-                                <div class="col-md-8 mb-0">
-                                    <h6>
-                                        <a href="shop-product-right.html">Pepperidge Farm Farmhouse Hearty White Bread</a>
-                                    </h6>
-                                    <div class="product-rate-cover">
-                                        <div class="product-rate d-inline-block">
-                                            <div class="product-rating" style="width: 90%"></div>
-                                        </div>
-                                        <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                    </div>
-                                    <div class="product-price">
-                                        <span>$32.85</span>
-                                        <span class="old-price">$33.8</span>
-                                    </div>
-                                </div>
-                            </article>
-                            <article class="row align-items-center hover-up">
-                                <figure class="col-md-4 mb-0">
-                                    <a href="shop-product-right.html"><img src="{{ asset('frontend/assets/imgs/shop/thumbnail-8.jpg') }}" alt="" /></a>
-                                </figure>
-                                <div class="col-md-8 mb-0">
-                                    <h6>
-                                        <a href="shop-product-right.html">Organic Frozen Triple Berry Blend</a>
-                                    </h6>
-                                    <div class="product-rate-cover">
-                                        <div class="product-rate d-inline-block">
-                                            <div class="product-rating" style="width: 90%"></div>
-                                        </div>
-                                        <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                    </div>
-                                    <div class="product-price">
-                                        <span>$32.85</span>
-                                        <span class="old-price">$33.8</span>
-                                    </div>
-                                </div>
-                            </article>
-                            <article class="row align-items-center hover-up">
-                                <figure class="col-md-4 mb-0">
-                                    <a href="shop-product-right.html"><img src="{{ asset('frontend/assets/imgs/shop/thumbnail-9.jpg') }}" alt="" /></a>
-                                </figure>
-                                <div class="col-md-8 mb-0">
-                                    <h6>
-                                        <a href="shop-product-right.html">Oroweat Country Buttermilk Bread</a>
-                                    </h6>
-                                    <div class="product-rate-cover">
-                                        <div class="product-rate d-inline-block">
-                                            <div class="product-rating" style="width: 90%"></div>
-                                        </div>
-                                        <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                    </div>
-                                    <div class="product-price">
-                                        <span>$32.85</span>
-                                        <span class="old-price">$33.8</span>
-                                    </div>
-                                </div>
-                            </article>
+                           
+
+  @foreach($new as $item)                   
+    <article class="row align-items-center hover-up">
+        <figure class="col-md-4 mb-0">
+            <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
+        </figure>
+        <div class="col-md-8 mb-0">
+            <h6>
+                <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"> {{ $item->product_name }} </a>
+            </h6>
+            <div class="product-rate-cover">
+                <div class="product-rate d-inline-block">
+                    <div class="product-rating" style="width: 90%"></div>
+                </div>
+                <span class="font-small ml-5 text-muted"> (4.0)</span>
+            </div>
+             @if($item->discount_price == NULL)
+                     <div class="product-price">
+                        <span>${{ $item->selling_price }}</span>
+                       
+                    </div>
+
+                    @else
+                    <div class="product-price">
+                        <span>${{ $item->discount_price }}</span>
+                        <span class="old-price">${{ $item->selling_price }}</span>
+                    </div>
+                    @endif
+        </div>
+    </article>
+    @endforeach
+
+
+                           
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-4 col-md-6 mb-sm-5 mb-md-0 d-none d-xl-block wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
                         <h4 class="section-title style-1 mb-30 animated animated"> Special Deals </h4>
                         <div class="product-list-small animated animated">
-                            <article class="row align-items-center hover-up">
-                                <figure class="col-md-4 mb-0">
-                                    <a href="shop-product-right.html"><img src="{{ asset('frontend/assets/imgs/shop/thumbnail-10.jpg') }}" alt="" /></a>
-                                </figure>
-                                <div class="col-md-8 mb-0">
-                                    <h6>
-                                        <a href="shop-product-right.html">Foster Farms Takeout Crispy Classic Buffalo Wings</a>
-                                    </h6>
-                                    <div class="product-rate-cover">
-                                        <div class="product-rate d-inline-block">
-                                            <div class="product-rating" style="width: 90%"></div>
-                                        </div>
-                                        <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                    </div>
-                                    <div class="product-price">
-                                        <span>$32.85</span>
-                                        <span class="old-price">$33.8</span>
-                                    </div>
-                                </div>
-                            </article>
-                            <article class="row align-items-center hover-up">
-                                <figure class="col-md-4 mb-0">
-                                    <a href="shop-product-right.html"><img src="{{ asset('frontend/assets/imgs/shop/thumbnail-11.jpg') }}" alt="" /></a>
-                                </figure>
-                                <div class="col-md-8 mb-0">
-                                    <h6>
-                                        <a href="shop-product-right.html">Angie’s Boomchickapop Sweet & Salty Kettle Corn</a>
-                                    </h6>
-                                    <div class="product-rate-cover">
-                                        <div class="product-rate d-inline-block">
-                                            <div class="product-rating" style="width: 90%"></div>
-                                        </div>
-                                        <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                    </div>
-                                    <div class="product-price">
-                                        <span>$32.85</span>
-                                        <span class="old-price">$33.8</span>
-                                    </div>
-                                </div>
-                            </article>
-                            <article class="row align-items-center hover-up">
-                                <figure class="col-md-4 mb-0">
-                                    <a href="shop-product-right.html"><img src="{{ asset('frontend/assets/imgs/shop/thumbnail-12.jpg') }}" alt="" /></a>
-                                </figure>
-                                <div class="col-md-8 mb-0">
-                                    <h6>
-                                        <a href="shop-product-right.html">All Natural Italian-Style Chicken Meatballs</a>
-                                    </h6>
-                                    <div class="product-rate-cover">
-                                        <div class="product-rate d-inline-block">
-                                            <div class="product-rating" style="width: 90%"></div>
-                                        </div>
-                                        <span class="font-small ml-5 text-muted"> (4.0)</span>
-                                    </div>
-                                    <div class="product-price">
-                                        <span>$32.85</span>
-                                        <span class="old-price">$33.8</span>
-                                    </div>
-                                </div>
-                            </article>
+                            
+
+  @foreach($special_deals as $item)                   
+    <article class="row align-items-center hover-up">
+        <figure class="col-md-4 mb-0">
+            <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"><img src="{{ asset( $item->product_thambnail ) }}" alt="" /></a>
+        </figure>
+        <div class="col-md-8 mb-0">
+            <h6>
+                <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}"> {{ $item->product_name }} </a>
+            </h6>
+            <div class="product-rate-cover">
+                <div class="product-rate d-inline-block">
+                    <div class="product-rating" style="width: 90%"></div>
+                </div>
+                <span class="font-small ml-5 text-muted"> (4.0)</span>
+            </div>
+             @if($item->discount_price == NULL)
+                     <div class="product-price">
+                        <span>${{ $item->selling_price }}</span>
+                       
+                    </div>
+
+                    @else
+                    <div class="product-price">
+                        <span>${{ $item->discount_price }}</span>
+                        <span class="old-price">${{ $item->selling_price }}</span>
+                    </div>
+                    @endif
+        </div>
+    </article>
+    @endforeach
+
+
+
+                            
                         </div>
                     </div>
                 </div>

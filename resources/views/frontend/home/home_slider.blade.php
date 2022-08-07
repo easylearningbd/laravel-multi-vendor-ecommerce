f28bf87a3b0acaf17ec8d36b2767f110276139d8
easylearningbd <elebdsoft@gmail.com>
2022-08-07 06:04:37 +0600

    101 Frontend Dynamic Main Sliders

--- resources/views/frontend/home/home_slider.blade.php
+++ resources/views/frontend/home/home_slider.blade.php
@@ -1,33 +1,27 @@
+     @php
+
+    $slider = App\Models\Slider::orderBy('slider_title','ASC')->get();
+    @endphp
+
  <section class="home-slider position-relative mb-30">
             <div class="container">
                 <div class="home-slide-cover mt-30">
                     <div class="hero-slider-1 style-4 dot-style-1 dot-style-1-position-1">
-                        <div class="single-hero-slider single-animation-wrap" style="background-image: url({{ asset('frontend/assets/imgs/slider/slider-1.png') }})">
+
+                    @foreach($slider as $item)
+                        <div class="single-hero-slider single-animation-wrap" style="background-image: url({{ asset($item->slider_image ) }})">
                             <div class="slider-content">
                                 <h1 class="display-2 mb-40">
-                                    Don’t miss amazing<br />
-                                    grocery deals
+                                    {{ $item->slider_title }}
                                 </h1>
-                                <p class="mb-65">Sign up for the daily newsletter</p>
+                                <p class="mb-65">{{ $item->short_title }}</p>
                                 <form class="form-subcriber d-flex">
                                     <input type="email" placeholder="Your emaill address" />
                                     <button class="btn" type="submit">Subscribe</button>
                                 </form>
                             </div>
                         </div>
-                        <div class="single-hero-slider single-animation-wrap" style="background-image: url({{ asset('frontend/assets/imgs/slider/slider-2.png') }})">
-                            <div class="slider-content">
-                                <h1 class="display-2 mb-40">
-                                    Fresh Vegetables<br />
-                                    Big discount
-                                </h1>
-                                <p class="mb-65">Save up to 50% off on your first order</p>
-                                <form class="form-subcriber d-flex">
-                                    <input type="email" placeholder="Your emaill address" />
-                                    <button class="btn" type="submit">Subscribe</button>
-                                </form>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                     <div class="slider-arrow hero-slider-1-arrow"></div>
                 </div>

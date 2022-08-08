57cbe6843b639103b9d0bfeb28b87a70e883668b
easylearningbd <elebdsoft@gmail.com>
2022-08-08 23:54:17 +0600

    119  Display Frontend Vendor List in Home Page.

--- resources/views/frontend/home/home_vendor_list.blade.php
+++ resources/views/frontend/home/home_vendor_list.blade.php
@@ -1,4 +1,9 @@
 
+
+@php
+$vendors = App\Models\User::where('status','active')->where('role','vendor')->orderBy('id','DESC')->limit(4)->get();
+@endphp
+
 <div class="container">
 
       <div class="section-title wow animate__animated animate__fadeIn" data-wow-delay="0">
@@ -8,15 +13,18 @@
                         <i class="fi-rs-angle-right"></i>
                     </a>
                 </div>
-
+ 
 
  <div class="row vendor-grid">
+
+
+               @foreach($vendors as $vendor)
                     <div class="col-lg-3 col-md-6 col-12 col-sm-6 justify-content-center">
                         <div class="vendor-wrap mb-40">
                             <div class="vendor-img-action-wrap">
                                 <div class="vendor-img">
                                     <a href="vendor-details-1.html">
-                                        <img class="default-img" src="{{ asset('frontend/assets/imgs/vendor/vendor-1.png') }}" alt="" />
+     <img class="default-img" src="{{ (!empty($vendor->photo)) ? url('upload/vendor_images/'.$vendor->photo):url('upload/no_image.jpg') }}" style="width:120px;height: 120px;" alt="" />
                                     </a>
                                 </div>
                                 <div class="product-badges product-badges-position product-badges-mrg">
@@ -27,12 +35,15 @@
                                 <div class="d-flex justify-content-between align-items-end mb-30">
                                     <div>
                                         <div class="product-category">
-                                            <span class="text-muted">Since 2012</span>
+                                            <span class="text-muted">Since {{ $vendor->vendor_join }}</span>
                                         </div>
-                                        <h4 class="mb-5"><a href="vendor-details-1.html">Nature Food</a></h4>
+                                        <h4 class="mb-5"><a href="vendor-details-1.html">{{ $vendor->name }}</a></h4>
                                         <div class="product-rate-cover">
-                                           
-                                           <span class="font-small total-product">380 products</span>
+              @php
+ $products = App\Models\Product::where('vendor_id',$vendor->id)->get();
+              @endphp                             
+      
+                                           <span class="font-small total-product">{{ count($products) }} products</span>
                                         </div>
                                     </div>
                                     
@@ -40,7 +51,7 @@
                                 <div class="vendor-info mb-30">
                                     <ul class="contact-infor text-muted">
                                         
-                                        <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-contact.svg') }}" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
+                                        <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-contact.svg') }}" alt="" /><strong>Call Us:</strong><span>{{ $vendor->phone }}</span></li>
                                     </ul>
                                 </div>
                                 <a href="vendor-details-1.html" class="btn btn-xs">Visit Store <i class="fi-rs-arrow-small-right"></i></a>
@@ -48,117 +59,10 @@
                         </div>
                     </div>
                     <!--end vendor card-->
-                    <div class="col-lg-3 col-md-6 col-12 col-sm-6 justify-content-center">
-                        <div class="vendor-wrap mb-40">
-                            <div class="vendor-img-action-wrap">
-                                <div class="vendor-img">
-                                    <a href="vendor-details-1.html">
-                                        <img class="default-img" src="{{ asset('frontend/assets/imgs/vendor/vendor-2.png') }}" alt="" />
-                                    </a>
-                                </div>
-                                <div class="product-badges product-badges-position product-badges-mrg">
-                                    <span class="hot">Mall</span>
-                                </div>
-                            </div>
-                            <div class="vendor-content-wrap">
-                                <div class="d-flex justify-content-between align-items-end mb-30">
-                                    <div>
-                                        <div class="product-category">
-                                            <span class="text-muted">Since 2012</span>
-                                        </div>
-                                        <h4 class="mb-5"><a href="vendor-details-1.html">Nature Food</a></h4>
-                                        <div class="product-rate-cover">
-                                           
-                                           <span class="font-small total-product">380 products</span>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                <div class="vendor-info mb-30">
-                                    <ul class="contact-infor text-muted">
-                                        
-                                        <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-contact.svg') }}" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
-                                    </ul>
-                                </div>
-                                <a href="vendor-details-1.html" class="btn btn-xs">Visit Store <i class="fi-rs-arrow-small-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end vendor card-->
-                    <div class="col-lg-3 col-md-6 col-12 col-sm-6 justify-content-center">
-                        <div class="vendor-wrap mb-40">
-                            <div class="vendor-img-action-wrap">
-                                <div class="vendor-img">
-                                    <a href="vendor-details-1.html">
-                                        <img class="default-img" src="{{ asset('frontend/assets/imgs/vendor/vendor-3.png') }}" alt="" />
-                                    </a>
-                                </div>
-                                <div class="product-badges product-badges-position product-badges-mrg">
-                                    <span class="hot">Mall</span>
-                                </div>
-                            </div>
-                            <div class="vendor-content-wrap">
-                                <div class="d-flex justify-content-between align-items-end mb-30">
-                                    <div>
-                                        <div class="product-category">
-                                            <span class="text-muted">Since 2012</span>
-                                        </div>
-                                        <h4 class="mb-5"><a href="vendor-details-1.html">Nature Food</a></h4>
-                                        <div class="product-rate-cover">
-                                           
-                                           <span class="font-small total-product">380 products</span>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                <div class="vendor-info mb-30">
-                                    <ul class="contact-infor text-muted">
-                                        
-                                        <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-contact.svg') }}" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
-                                    </ul>
-                                </div>
-                                <a href="vendor-details-1.html" class="btn btn-xs">Visit Store <i class="fi-rs-arrow-small-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end vendor card-->
-                    <div class="col-lg-3 col-md-6 col-12 col-sm-6 justify-content-center">
-                        <div class="vendor-wrap mb-40">
-                            <div class="vendor-img-action-wrap">
-                                <div class="vendor-img">
-                                    <a href="vendor-details-1.html">
-                                        <img class="default-img" src="{{ asset('frontend/assets/imgs/vendor/vendor-4.png') }}" alt="" />
-                                    </a>
-                                </div>
-                                <div class="product-badges product-badges-position product-badges-mrg">
-                                    <span class="hot">Mall</span>
-                                </div>
-                            </div>
-                            <div class="vendor-content-wrap">
-                                <div class="d-flex justify-content-between align-items-end mb-30">
-                                    <div>
-                                        <div class="product-category">
-                                            <span class="text-muted">Since 2012</span>
-                                        </div>
-                                        <h4 class="mb-5"><a href="vendor-details-1.html">Nature Food</a></h4>
-                                        <div class="product-rate-cover">
-                                           
-                                           <span class="font-small total-product">380 products</span>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                                <div class="vendor-info mb-30">
-                                    <ul class="contact-infor text-muted">
-                                        
-                                        <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-contact.svg') }}" alt="" /><strong>Call Us:</strong><span>(+91) - 540-025-124553</span></li>
-                                    </ul>
-                                </div>
-                                <a href="vendor-details-1.html" class="btn btn-xs">Visit Store <i class="fi-rs-arrow-small-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!--end vendor card-->
+                    @endforeach
+
+
+                   
                 
                 </div> 
             </div>

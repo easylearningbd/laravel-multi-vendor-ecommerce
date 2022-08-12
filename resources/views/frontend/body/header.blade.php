e021bc0fe385c54a8bd3b66057d1d8ed717421b4
easylearningbd <elebdsoft@gmail.com>
2022-08-13 01:16:03 +0600

    139 Mini Cart Read Cart Product With Ajax Part 2

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -109,6 +109,10 @@
                     </a>
                     <a href="shop-wishlist.html"><span class="lable">Wishlist</span></a>
                 </div>
+
+
+
+
                 <div class="header-action-icon-2">
                     <a class="mini-cart-icon" href="shop-cart.html">
                         <img alt="Nest" src="{{ asset('frontend/assets/imgs/theme/icons/icon-cart.svg') }}" />
@@ -116,32 +120,19 @@
                     </a>
                     <a href="shop-cart.html"><span class="lable">Cart</span></a>
                     <div class="cart-dropdown-wrap cart-dropdown-hm2">
-                        <ul>
-                            <li>
-                                <div class="shopping-cart-img">
-                                    <a href="shop-product-right.html"><img alt="Nest" src="{{ asset('frontend/assets/imgs/shop/thumbnail-3.jpg') }}" /></a>
-                                </div>
-                                <div class="shopping-cart-title">
-                                    <h4><a href="shop-product-right.html">Daisy Casual Bag</a></h4>
-                                    <h4><span>1 × </span>$800.00</h4>
-                                </div>
-                                <div class="shopping-cart-delete">
-                                    <a href="#"><i class="fi-rs-cross-small"></i></a>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="shopping-cart-img">
-                                    <a href="shop-product-right.html"><img alt="Nest" src="{{ asset('frontend/assets/imgs/shop/thumbnail-2.jpg') }}" /></a>
-                                </div>
-                                <div class="shopping-cart-title">
-                                    <h4><a href="shop-product-right.html">Corduroy Shirts</a></h4>
-                                    <h4><span>1 × </span>$3200.00</h4>
-                                </div>
-                                <div class="shopping-cart-delete">
-                                    <a href="#"><i class="fi-rs-cross-small"></i></a>
-                                </div>
-                            </li>
-                        </ul>
+                        
+
+         <!--   // mini cart start with ajax -->
+         <div id="miniCart">
+            
+         </div>
+
+          <!--   // End mini cart start with ajax -->
+           
+
+
+
+
                         <div class="shopping-cart-footer">
                             <div class="shopping-cart-total">
                                 <h4>Total <span>$4000.00</span></h4>
--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -236,12 +236,37 @@
         url: '/product/mini/cart',
         dataType: 'json',
         success:function(response){
-            console.log(response)
+            // console.log(response)
+
+
+        var miniCart = ""
+
+        $.each(response.carts, function(key,value){
+           miniCart += ` <ul>
+            <li>
+                <div class="shopping-cart-img">
+                    <a href="shop-product-right.html"><img alt="Nest" src="/${value.options.image} " style="width:50px;height:50px;" /></a>
+                </div>
+                <div class="shopping-cart-title" style="margin: -73px 74px 14px; width" 146px;>
+                    <h4><a href="shop-product-right.html"> ${value.name} </a></h4>
+                    <h4><span>${value.qty} × </span>${value.price}</h4>
+                </div>
+                <div class="shopping-cart-delete" style="margin: -85px 1px 0px;">
+                    <a href="#"><i class="fi-rs-cross-small"></i></a>
+                </div>
+            </li> 
+        </ul>
+        <hr><br>  
+               `  
+          });
+
+            $('#miniCart').html(miniCart);
+
         }
 
     })
  }
-
+  miniCart();
 
 
 </script>

acf977d56deac79da0847575eadbbe742e10c4ee
easylearningbd <elebdsoft@gmail.com>
2022-08-20 02:03:54 +0600

    177 Coupon Remove & Discount With Ajax Part 2

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -160,6 +160,19 @@ class CartController extends Controller
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty -1);
 
+        if(Session::has('coupon')){
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name',$coupon_name)->first();
+           
+           Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name, 
+                'coupon_discount' => $coupon->coupon_discount, 
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100 )
+            ]); 
+        }
+
+
         return response()->json('Decrement');
 
     }// End Method
@@ -169,6 +182,18 @@ class CartController extends Controller
 
         $row = Cart::get($rowId);
         Cart::update($rowId, $row->qty +1);
+
+          if(Session::has('coupon')){
+            $coupon_name = Session::get('coupon')['coupon_name'];
+            $coupon = Coupon::where('coupon_name',$coupon_name)->first();
+           
+           Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name, 
+                'coupon_discount' => $coupon->coupon_discount, 
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100), 
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100 )
+            ]); 
+        }
 
         return response()->json('Increment');
 

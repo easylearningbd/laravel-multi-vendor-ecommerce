57e192c4fe8ea17648e20937d632a60ddaa21070
easylearningbd <elebdsoft@gmail.com>
2022-08-22 03:34:30 +0600

    184 Checkout Page Design and Setup Part 5

--- resources/views/frontend/payment/cash.blade.php
+++ resources/views/frontend/payment/cash.blade.php
@@ -1,7 +1,156 @@
 @extends('frontend.master_dashboard')
 @section('main')
 
+ 
+ <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a> 
+                    <span></span> Cash On Delivery Payment
+                </div>
+            </div>
+        </div>
+        <div class="container mb-80 mt-50">
+            <div class="row">
+                <div class="col-lg-8 mb-40">
+                    <h3 class="heading-2 mb-10"> Cash On Delivery</h3>
+                    <div class="d-flex justify-content-between">
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+
+
+                	<div class="border p-40 cart-totals ml-30 mb-50">
+    <div class="d-flex align-items-end justify-content-between mb-30">
+        <h4>Your Order</h4>
+       
+    </div>
+    <div class="divider-2 mb-30"></div>
+    <div class="table-responsive order_table checkout"> 
+
+ <table class="table no-border">
+        <tbody>
+            <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Subtotal</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h4 class="text-brand text-end">$12.31</h4>
+                </td>
+            </tr>
+            
+            <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Coupn Name</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h6 class="text-brand text-end">EASYLEA</h6>
+                </td>
+            </tr>
+
+              <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Coupon Discount</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h4 class="text-brand text-end">$12.31</h4>
+                </td>
+            </tr>
+
+              <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Grand Total</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h4 class="text-brand text-end">$12.31</h4>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+
+
+    </div>
+</div>
+    
+     
+                </div> <!-- // end lg md 6 -->
+
+                
+<div class="col-lg-6">
+<div class="border p-40 cart-totals ml-30 mb-50">
+    <div class="d-flex align-items-end justify-content-between mb-30">
+        <h4>Make Cash Payment </h4>
+         
+    </div>
+    <div class="divider-2 mb-30"></div>
+    <div class="table-responsive order_table checkout">
+         
+
+ <table class="table no-border">
+        <tbody>
+            <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Subtotal</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h4 class="text-brand text-end">$12.31</h4>
+                </td>
+            </tr>
+            
+            <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Coupn Name</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h6 class="text-brand text-end">EASYLEA</h6>
+                </td>
+            </tr>
+
+              <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Coupon Discount</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h4 class="text-brand text-end">$12.31</h4>
+                </td>
+            </tr>
+
+              <tr>
+                <td class="cart_total_label">
+                    <h6 class="text-muted">Grand Total</h6>
+                </td>
+                <td class="cart_total_amount">
+                    <h4 class="text-brand text-end">$12.31</h4>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+
+
+    </div>
+</div>
+                     
+
+
+                </div>
+            </div>
+        </div>
+ 
+
 
 
 
 @endsection
+
+
+
+

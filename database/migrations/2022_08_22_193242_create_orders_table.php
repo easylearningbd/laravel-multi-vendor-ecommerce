e852d67fa63f99ae2e40872d9cbc159aaf93accf
easylearningbd <elebdsoft@gmail.com>
2022-08-23 01:56:06 +0600

    190 Stripe Payment Gateway  Part 2

---
+++ database/migrations/2022_08_22_193242_create_orders_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('division_id');
+            $table->unsignedBigInteger('district_id');
+            $table->unsignedBigInteger('state_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('adress')->nullable();
+            $table->string('post_code')->nullable();
+            $table->text('notes')->nullable();
+            $table->string('payment_type')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->string('currency');
+            $table->float('amount',8,2);
+            $table->string('order_number')->nullable();
+            $table->string('invoice_no');
+            $table->string('order_date');
+            $table->string('order_month');
+            $table->string('order_year');
+            $table->string('confirmed_date')->nullable();
+            $table->string('processing_date')->nullable();
+            $table->string('picked_date')->nullable();
+            $table->string('shipped_date')->nullable();
+            $table->string('delivered_date')->nullable();
+            $table->string('cancel_date')->nullable();
+            $table->string('return_date')->nullable();
+            $table->string('return_reason')->nullable();
+            $table->string('status'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2022_08_22_194505_create_order_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedBigInteger('product_id');
+            $table->string('vendor_id')->nullable();
+            $table->string('color')->nullable();
+            $table->string('size')->nullable();
+            $table->string('qty');
+            $table->float('price',8,2); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+};

4f0e4cc18333e9ed3f17964819c2229e3203442a
easylearningbd <elebdsoft@gmail.com>
2022-08-23 01:25:57 +0600

    189 Stripe Payment Gateway  Part 1

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -7,12 +7,27 @@ use Illuminate\Http\Request;
 
 class StripeController extends Controller
 {
-    public function StripeOrder(){
+    public function StripeOrder(Request $request){
+
+        \Stripe\Stripe::setApiKey('sk_test_51IUTWzALc6pn5BvMjaRW9STAvY4pLiq1dNViHoh5KtqJc9Bx7d4WKlCcEdHOJdg3gCcC2F19cDxUmCBJekGSZXte00RN2Fc4vm');
+
+ 
+        $token = $_POST['stripeToken'];
+
+        $charge = \Stripe\Charge::create([
+          'amount' => 999*100,
+          'currency' => 'usd',
+          'description' => 'Easy Mulit Vendor Shop',
+          'source' => $token,
+          'metadata' => ['order_id' => '6735'],
+        ]);
+
+        dd($charge);
 
 
     }// End Method 
 
 
-    
+
 }
  
--- resources/views/frontend/payment/stripe.blade.php
+++ resources/views/frontend/payment/stripe.blade.php
@@ -166,7 +166,7 @@
 
 <script type="text/javascript">
     // Create a Stripe client.
-var stripe = Stripe('pk_test_zuURZYgtzc5QCrAq3ITN7h2M007nb4GJy9');
+var stripe = Stripe('pk_test_51IUTWzALc6pn5BvMAUegqRHV0AAokjG7ZuV6RWcj5rxB9KCAwamgtWpw9T4maGAe34WmDkD6LSn1Yge3nzex6gYk004pILHsNh');
 // Create an instance of Elements.
 var elements = stripe.elements();
 // Custom styling can be passed to options when creating an Element.

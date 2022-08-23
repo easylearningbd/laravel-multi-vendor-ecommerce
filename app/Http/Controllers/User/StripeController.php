c7acd1537f558ec343a374623fd2c27866b6acee
easylearningbd <elebdsoft@gmail.com>
2022-08-23 14:54:15 +0600

    197 Send Order Mail After Complete Stripe Payment Part 2

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -10,6 +10,8 @@ use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Support\Facades\Session;
 use Carbon\Carbon;
 use Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderMail;
 
 class StripeController extends Controller
 {
@@ -76,6 +78,8 @@ class StripeController extends Controller
             'email' => $invoice->email,
 
         ];
+
+        Mail::to($request->email)->send(new OrderMail($data));
 
         // End Send Email 
 
@@ -153,6 +157,27 @@ class StripeController extends Controller
             'created_at' => Carbon::now(),  
 
         ]);
+
+
+
+  // Start Send Email
+
+        $invoice = Order::findOrFail($order_id);
+
+        $data = [
+
+            'invoice_no' => $invoice->invoice_no,
+            'amount' => $total_amount,
+            'name' => $invoice->name,
+            'email' => $invoice->email,
+
+        ];
+
+        Mail::to($request->email)->send(new OrderMail($data));
+
+        // End Send Email 
+
+
 
         $carts = Cart::content();
         foreach($carts as $cart){

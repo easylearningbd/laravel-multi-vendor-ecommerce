47c9daf2707d2dde4dfc42bd845f77552ca2e3d1
easylearningbd <elebdsoft@gmail.com>
2022-08-25 05:17:19 +0600

    208 Make Order Invoice & Generate PDF Part 2

--- resources/views/frontend/order/order_invoice.blade.php
+++ resources/views/frontend/order/order_invoice.blade.php
@@ -70,20 +70,24 @@
     <tr>
         <td>
           <p class="font" style="margin-left: 20px;">
-           <strong>Name:</strong> Name <br>
-           <strong>Email:</strong> Email <br>
-           <strong>Phone:</strong> Phone <br>
-            
-           <strong>Address:</strong> Address <br>
-           <strong>Post Code:</strong> Post Code
+           <strong>Name:</strong> {{ $order->name }} <br>
+           <strong>Email:</strong> {{ $order->email }} <br>
+           <strong>Phone:</strong> {{ $order->phone }} <br>
+            @php
+            $div =  $order->division->division_name; 
+            $dis =  $order->district->district_name;
+            $state = $order->state->state_name; 
+            @endphp
+           <strong>Address:</strong> {{ $order->adress }} / {{$div}} / {{ $dis }}/ {{ $state }}<br>
+           <strong>Post Code:</strong> {{ $order->post_code }}
          </p>
         </td>
         <td>
           <p class="font">
-            <h3><span style="color: green;">Invoice:</span> #Invoice</h3>
-            Order Date: Order Date <br>
-             Delivery Date: Delivery Date <br>
-            Payment Type : Payment Type </span>
+            <h3><span style="color: green;">Invoice:</span> #{{ $order->invoice_no }}</h3>
+            Order Date: {{ $order->order_date }} <br>
+             Delivery Date: {{ $order->delivered_date }} <br>
+            Payment Type : {{ $order->payment_method }} </span>
          </p>
         </td>
     </tr>
@@ -101,36 +105,50 @@
         <th>Color</th>
         <th>Code</th>
         <th>Quantity</th>
-        <th>Unit Price </th>
+        <th>Vendor</th>
         <th>Total </th>
       </tr>
     </thead>
     <tbody>
 
-     
+     @foreach($orderItem as $item)
       <tr class="font">
         <td align="center">
-            <img src=" " height="60px;" width="60px;" alt="">
+            <img src="{{ public_path($item->product->product_thambnail) }}" height="50px;" width="50px;" alt="">
         </td>
-        <td align="center">product_name_en</td>
-        <td align="center">
-            
-        </td>
-        <td align="center">color</td>
-        <td align="center">product_code</td>
-        <td align="center">qty</td>
-        <td align="center">price Tk</td>
-        <td align="center">price Tk</td>
+        <td align="center">{{ $item->product->product_name }}</td>
+       
+         @if($item->color == NULL)
+         <td align="center"> ...</td>
+         @else
+          <td align="center"> {{ $item->color }}</td>
+         @endif
+
+         @if($item->size == NULL)
+         <td align="center"> ...</td>
+         @else
+          <td align="center"> {{ $item->size }}</td>
+         @endif
+        <td align="center">{{ $item->product->product_code }}</td>
+        <td align="center">{{ $item->qty }}</td>
+
+         @if($item->vendor_id == NULL)
+         <td align="center">Owner</td>
+          @else
+          <td align="center">{{ $item->product->vendor->name }}</td>
+          @endif
+        
+        <td align="center">${{ $item->price }}</td>
       </tr>
-      
+      @endforeach
     </tbody>
   </table>
   <br>
   <table width="100%" style=" padding:0 10px 0 10px;">
     <tr>
         <td align="right" >
-            <h2><span style="color: green;">Subtotal:</span> Subtotal tk</h2>
-            <h2><span style="color: green;">Total:</span> Total tk</h2>
+            <h2><span style="color: green;">Subtotal:</span>${{ $order->amount }}</h2>
+            <h2><span style="color: green;">Total:</span> ${{ $order->amount }}</h2>
             {{-- <h2><span style="color: green;">Full Payment PAID</h2> --}}
         </td>
     </tr>

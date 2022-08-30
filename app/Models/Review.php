eecb737af8df8617b9bbb547e4ad318f0b0e10c6
easylearningbd <elebdsoft@gmail.com>
2022-08-30 15:47:15 +0600

    244 Product Review System Part 4

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,4 +9,9 @@ class Review extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+
 }
--- resources/views/frontend/product/product_details.blade.php
+++ resources/views/frontend/product/product_details.blade.php
@@ -336,28 +336,52 @@
             <div class="col-lg-8">
                 <h4 class="mb-30">Customer questions & answers</h4>
                 <div class="comment-list">
+@php
+$reviews = App\Models\Review::where('product_id',$product->id)->latest()->limit(5)->get();
+@endphp
 
+    @foreach($reviews as $item)
 
-                    <div class="single-comment justify-content-between d-flex mb-30">
-                        <div class="user justify-content-between d-flex">
-                            <div class="thumb text-center">
-                                <img src="assets/imgs/blog/author-2.png" alt="" />
-                                <a href="#" class="font-heading text-brand">Sienna</a>
-                            </div>
-                            <div class="desc">
-                                <div class="d-flex justify-content-between mb-10">
-                                    <div class="d-flex align-items-center">
-                                        <span class="font-xs text-muted">December 4, 2022 at 3:12 pm </span>
-                                    </div>
-                                    <div class="product-rate d-inline-block">
-                                        <div class="product-rating" style="width: 100%"></div>
-                                    </div>
-                                </div>
-                                <p class="mb-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, suscipit exercitationem accusantium obcaecati quos voluptate nesciunt facilis itaque modi commodi dignissimos sequi repudiandae minus ab deleniti totam officia id incidunt? <a href="#" class="reply">Reply</a></p>
-                            </div>
-                        </div>
+    @if($item->status == 0)
+
+    @else 
+
+    <div class="single-comment justify-content-between d-flex mb-30">
+        <div class="user justify-content-between d-flex">
+            <div class="thumb text-center">
+                <img src="{{ (!empty($item->user->photo)) ? url('upload/user_images/'.$item->user->photo):url('upload/no_image.jpg') }}" alt="" />
+                <a href="#" class="font-heading text-brand">{{ $item->user->name }}</a>
+            </div>
+            <div class="desc">
+                <div class="d-flex justify-content-between mb-10">
+                    <div class="d-flex align-items-center">
+                        <span class="font-xs text-muted"> {{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }} </span>
                     </div>
+                    <div class="product-rate d-inline-block">
 
+@if($item->rating == NULL)
+@elseif($item->rating == 1)
+ <div class="product-rating" style="width: 20%"></div>
+@elseif($item->rating == 2)
+ <div class="product-rating" style="width: 40%"></div>
+@elseif($item->rating == 3)
+<div class="product-rating" style="width: 60%"></div>
+@elseif($item->rating == 4)
+ <div class="product-rating" style="width: 80%"></div>
+@elseif($item->rating == 5)
+ <div class="product-rating" style="width: 100%"></div>
+@endif
+                    </div>
+                </div>
+                <p class="mb-10">{{ $item->comment }} <a href="#" class="reply">Reply</a></p>
+            </div>
+        </div>
+    </div>
+
+     @endif
+
+
+    @endforeach
                   
                  
                 </div>

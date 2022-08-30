4a4b8378fc318ef98f725e7f4379e33441821f90
easylearningbd <elebdsoft@gmail.com>
2022-08-30 16:01:25 +0600

    245 Product Review System Part 5

--- resources/views/frontend/product/product_details.blade.php
+++ resources/views/frontend/product/product_details.blade.php
@@ -48,10 +48,34 @@
                 <h2 class="title-detail" id="dpname"> {{ $product->product_name }} </h2>
                 <div class="product-detail-rating">
                     <div class="product-rate-cover text-end">
+
+@php
+
+$reviewcount = App\Models\Review::where('product_id',$product->id)->where('status',1)->latest()->get();
+
+$avarage = App\Models\Review::where('product_id',$product->id)->where('status',1)->avg('rating');
+@endphp
+
+
                         <div class="product-rate d-inline-block">
-                            <div class="product-rating" style="width: 90%"></div>
+       @if($avarage == 0)
+      
+       @elseif($avarage == 1 || $avarage < 2)                     
+    <div class="product-rating" style="width: 20%"></div>
+       @elseif($avarage == 2 || $avarage < 3)                     
+    <div class="product-rating" style="width: 40%"></div>
+       @elseif($avarage == 3 || $avarage < 4)                     
+    <div class="product-rating" style="width: 60%"></div>
+       @elseif($avarage == 4 || $avarage < 5)                     
+    <div class="product-rating" style="width: 80%"></div>
+       @elseif($avarage == 5 || $avarage < 5)                     
+    <div class="product-rating" style="width: 100%"></div>
+    @endif
                         </div>
-                        <span class="font-small ml-5 text-muted"> (32 reviews)</span>
+
+
+
+                        <span class="font-small ml-5 text-muted"> ({{ count($reviewcount)}} reviews)</span>
                     </div>
                 </div>
                 <div class="clearfix product-price-cover">
@@ -182,7 +206,7 @@
     <a class="nav-link" id="Vendor-info-tab" data-bs-toggle="tab" href="#Vendor-info">Vendor</a>
 </li>
 <li class="nav-item">
-    <a class="nav-link" id="Reviews-tab" data-bs-toggle="tab" href="#Reviews">Reviews (3)</a>
+    <a class="nav-link" id="Reviews-tab" data-bs-toggle="tab" href="#Reviews">Reviews ({{ count($reviewcount) }})</a>
 </li>
 </ul>
 <div class="tab-content shop_info_tab entry-main-content">

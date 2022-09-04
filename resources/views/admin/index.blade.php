5ae9ad4a5bccf98baa913c73b1b4a30338b954ca
easylearningbd <elebdsoft@gmail.com>
2022-09-04 14:52:45 +0600

    260 Update Admin Dashboard Part 2

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -158,7 +158,10 @@
  
 
   
+@php
 
+$orders = App\Models\Order::where('status','pending')->orderBy('id','DESC')->limit(10)->get();
+@endphp
 
 					  <div class="card radius-10">
 						<div class="card-body">
@@ -174,184 +177,34 @@
 								<table class="table align-middle mb-0">
 									<thead class="table-light">
 										<tr>
-											<th>Order id</th>
-											<th>Product</th>
-											<th>Customer</th>
+											<th>Sl</th>
 											<th>Date</th>
-											<th>Price</th>
-											<th>Status</th>
-											<th>Action</th>
+											<th>Invoice</th>
+											<th>Amount</th>
+											<th>Payment</th>
+											<th>Status</th> 
 										</tr>
 									</thead>
-									<tbody>
+	
+
+							<tbody>
+
+	@foreach($orders as $key => $order)								
 	<tr>
-		<td>#897656</td>
+		<td>{{ $key+1 }}</td>
+		 
+		<td>{{ $order->order_date }}</td>
+		<td>{{ $order->invoice_no }}</td>
+		<td>${{ $order->amount }}</td>
+		<td>{{ $order->payment_method }}</td>
 		<td>
-			<div class="d-flex align-items-center">
-				<div class="recent-product-img">
-					<img src="{{ asset('adminbackend/assets/images/icons/chair.png') }}" alt="">
-				</div>
-				<div class="ms-2">
-					<h6 class="mb-1 font-14">Light Blue Chair</h6>
-				</div>
-			</div>
+			<div class="badge rounded-pill bg-light-info text-info w-100"> 
+				{{ $order->status  }}</div>
 		</td>
-		<td>Brooklyn Zeo</td>
-		<td>12 Jul 2020</td>
-		<td>$64.00</td>
-		<td>
-			<div class="badge rounded-pill bg-light-info text-info w-100">In Progress</div>
-		</td>
-		<td>
-			<div class="d-flex order-actions">	<a href="javascript:;" class=""><i class="bx bx-cog"></i></a>
-				<a href="javascript:;" class="ms-4"><i class="bx bx-down-arrow-alt"></i></a>
-			</div>
-		</td>
+	 
 	</tr>
-	<tr>
-		<td>#987549</td>
-		<td>
-			<div class="d-flex align-items-center">
-				<div class="recent-product-img">
-					<img src="{{ asset('adminbackend/assets/images/icons/shoes.png') }}" alt="">
-				</div>
-				<div class="ms-2">
-					<h6 class="mb-1 font-14">Green Sport Shoes</h6>
-				</div>
-			</div>
-		</td>
-		<td>Martin Hughes</td>
-		<td>14 Jul 2020</td>
-		<td>$45.00</td>
-		<td>
-			<div class="badge rounded-pill bg-light-success text-success w-100">Completed</div>
-		</td>
-		<td>
-			<div class="d-flex order-actions">	<a href="javascript:;" class=""><i class="bx bx-cog"></i></a>
-				<a href="javascript:;" class="ms-4"><i class="bx bx-down-arrow-alt"></i></a>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td>#685749</td>
-		<td>
-			<div class="d-flex align-items-center">
-				<div class="recent-product-img">
-					<img src="{{ asset('adminbackend/assets/images/icons/headphones.png') }}" alt="">
-				</div>
-				<div class="ms-2">
-					<h6 class="mb-1 font-14">Red Headphone 07</h6>
-				</div>
-			</div>
-		</td>
-		<td>Shoan Stephen</td>
-		<td>15 Jul 2020</td>
-		<td>$67.00</td>
-		<td>
-			<div class="badge rounded-pill bg-light-danger text-danger w-100">Cancelled</div>
-		</td>
-		<td>
-			<div class="d-flex order-actions">	<a href="javascript:;" class=""><i class="bx bx-cog"></i></a>
-				<a href="javascript:;" class="ms-4"><i class="bx bx-down-arrow-alt"></i></a>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td>#887459</td>
-		<td>
-			<div class="d-flex align-items-center">
-				<div class="recent-product-img">
-					<img src="{{ asset('adminbackend/assets/images/icons/idea.png') }}" alt="">
-				</div>
-				<div class="ms-2">
-					<h6 class="mb-1 font-14">Mini Laptop Device</h6>
-				</div>
-			</div>
-		</td>
-		<td>Alister Campel</td>
-		<td>18 Jul 2020</td>
-		<td>$87.00</td>
-		<td>
-			<div class="badge rounded-pill bg-light-success text-success w-100">Completed</div>
-		</td>
-		<td>
-			<div class="d-flex order-actions">	<a href="javascript:;" class=""><i class="bx bx-cog"></i></a>
-				<a href="javascript:;" class="ms-4"><i class="bx bx-down-arrow-alt"></i></a>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td>#335428</td>
-		<td>
-			<div class="d-flex align-items-center">
-				<div class="recent-product-img">
-					<img src="{{ asset('adminbackend/assets/images/icons/user-interface.png') }}" alt="">
-				</div>
-				<div class="ms-2">
-					<h6 class="mb-1 font-14">Purple Mobile Phone</h6>
-				</div>
-			</div>
-		</td>
-		<td>Keate Medona</td>
-		<td>20 Jul 2020</td>
-		<td>$75.00</td>
-		<td>
-			<div class="badge rounded-pill bg-light-info text-info w-100">In Progress</div>
-		</td>
-		<td>
-			<div class="d-flex order-actions">	<a href="javascript:;" class=""><i class="bx bx-cog"></i></a>
-				<a href="javascript:;" class="ms-4"><i class="bx bx-down-arrow-alt"></i></a>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td>#224578</td>
-		<td>
-			<div class="d-flex align-items-center">
-				<div class="recent-product-img">
-					<img src="{{ asset('adminbackend/assets/images/icons/watch.png') }}" alt="">
-				</div>
-				<div class="ms-2">
-					<h6 class="mb-1 font-14">Smart Hand Watch</h6>
-				</div>
-			</div>
-		</td>
-		<td>Winslet Maya</td>
-		<td>22 Jul 2020</td>
-		<td>$80.00</td>
-		<td>
-			<div class="badge rounded-pill bg-light-danger text-danger w-100">Cancelled</div>
-		</td>
-		<td>
-			<div class="d-flex order-actions">	<a href="javascript:;" class=""><i class="bx bx-cog"></i></a>
-				<a href="javascript:;" class="ms-4"><i class="bx bx-down-arrow-alt"></i></a>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td>#447896</td>
-		<td>
-			<div class="d-flex align-items-center">
-				<div class="recent-product-img">
-					<img src="{{ asset('adminbackend/assets/images/icons/tshirt.png') }}" alt="">
-				</div>
-				<div class="ms-2">
-					<h6 class="mb-1 font-14">T-Shirt Blue</h6>
-				</div>
-			</div>
-		</td>
-		<td>Emy Jackson</td>
-		<td>28 Jul 2020</td>
-		<td>$96.00</td>
-		<td>
-			<div class="badge rounded-pill bg-light-success text-success w-100">Completed</div>
-		</td>
-		<td>
-			<div class="d-flex order-actions">	<a href="javascript:;" class=""><i class="bx bx-cog"></i></a>
-				<a href="javascript:;" class="ms-4"><i class="bx bx-down-arrow-alt"></i></a>
-			</div>
-											</td>
-										</tr>
+	@endforeach
+	 
 									</tbody>
 								</table>
 							</div>

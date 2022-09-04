b54317ea400213446fab1c5a7a762c81be73d02b
easylearningbd <elebdsoft@gmail.com>
2022-09-04 14:21:16 +0600

    259 Update Admin Dashboard Part 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,29 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
+@php
+	$date = date('d-m-y');
+	$today = App\Models\Order::where('order_date',$date)->sum('amount');
+
+	$month = date('F');
+	$month = App\Models\Order::where('order_month',$month)->sum('amount');
+
+
+	$year = date('Y');
+	$year = App\Models\Order::where('order_year',$year)->sum('amount');
+
+	$pending = App\Models\Order::where('status','pending')->get();
+
+	$vendor = App\Models\User::where('status','active')->where('role','vendor')->get();
+
+	$customer = App\Models\User::where('status','active')->where('role','user')->get();
+
+@endphp
+
+
+
+
+
 <div class="page-content">
 
 					<div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
@@ -8,7 +31,7 @@
 							<div class="card radius-10 bg-gradient-deepblue">
 							 <div class="card-body">
 								<div class="d-flex align-items-center">
-									<h5 class="mb-0 text-white">9526</h5>
+									<h5 class="mb-0 text-white">${{ $today }}USD</h5>
 									<div class="ms-auto">
                                         <i class='bx bx-cart fs-3 text-white'></i>
 									</div>
@@ -17,7 +40,7 @@
 									<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 								</div>
 								<div class="d-flex align-items-center text-white">
-									<p class="mb-0">Total Orders</p>
+									<p class="mb-0">Today's Sale</p>
 									<p class="mb-0 ms-auto">+4.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 								</div>
 							</div>
@@ -27,7 +50,7 @@
 							<div class="card radius-10 bg-gradient-orange">
 							<div class="card-body">
 								<div class="d-flex align-items-center">
-									<h5 class="mb-0 text-white">$8323</h5>
+									<h5 class="mb-0 text-white">${{ $month }}USD</h5>
 									<div class="ms-auto">
                                         <i class='bx bx-dollar fs-3 text-white'></i>
 									</div>
@@ -36,7 +59,7 @@
 									<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 								</div>
 								<div class="d-flex align-items-center text-white">
-									<p class="mb-0">Total Revenue</p>
+									<p class="mb-0">Monthly Sale</p>
 									<p class="mb-0 ms-auto">+1.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 								</div>
 							</div>
@@ -46,7 +69,7 @@
 							<div class="card radius-10 bg-gradient-ohhappiness">
 							<div class="card-body">
 								<div class="d-flex align-items-center">
-									<h5 class="mb-0 text-white">6200</h5>
+									<h5 class="mb-0 text-white">${{ $year }}USD</h5>
 									<div class="ms-auto">
                                         <i class='bx bx-group fs-3 text-white'></i>
 									</div>
@@ -55,7 +78,7 @@
 									<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 								</div>
 								<div class="d-flex align-items-center text-white">
-									<p class="mb-0">Visitors</p>
+									<p class="mb-0">Yearly Sale</p>
 									<p class="mb-0 ms-auto">+5.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 								</div>
 							</div>
@@ -65,7 +88,7 @@
 							<div class="card radius-10 bg-gradient-ibiza">
 							 <div class="card-body">
 								<div class="d-flex align-items-center">
-									<h5 class="mb-0 text-white">5630</h5>
+									<h5 class="mb-0 text-white">{{ count($pending) }}</h5>
 									<div class="ms-auto">
                                         <i class='bx bx-envelope fs-3 text-white'></i>
 									</div>
@@ -74,12 +97,61 @@
 									<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 								</div>
 								<div class="d-flex align-items-center text-white">
-									<p class="mb-0">Messages</p>
+									<p class="mb-0">Pending Orders</p>
 									<p class="mb-0 ms-auto">+2.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
 								</div>
 							</div>
 						 </div>
 						</div>
+
+
+
+	<div class="col">
+							<div class="card radius-10 bg-gradient-ibiza">
+							 <div class="card-body">
+								<div class="d-flex align-items-center">
+									<h5 class="mb-0 text-white">{{ count($vendor) }}</h5>
+									<div class="ms-auto">
+                                        <i class='bx bx-envelope fs-3 text-white'></i>
+									</div>
+								</div>
+								<div class="progress my-3 bg-light-transparent" style="height:3px;">
+									<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<div class="d-flex align-items-center text-white">
+									<p class="mb-0">Total Vendor </p>
+									<p class="mb-0 ms-auto">+2.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
+								</div>
+							</div>
+						 </div>
+						</div>
+
+
+
+
+							<div class="col">
+							<div class="card radius-10 bg-gradient-ibiza">
+							 <div class="card-body">
+								<div class="d-flex align-items-center">
+									<h5 class="mb-0 text-white">{{ count($customer) }}</h5>
+									<div class="ms-auto">
+                                        <i class='bx bx-envelope fs-3 text-white'></i>
+									</div>
+								</div>
+								<div class="progress my-3 bg-light-transparent" style="height:3px;">
+									<div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<div class="d-flex align-items-center text-white">
+									<p class="mb-0">Total User </p>
+									<p class="mb-0 ms-auto">+2.2%<span><i class='bx bx-up-arrow-alt'></i></span></p>
+								</div>
+							</div>
+						 </div>
+						</div>
+
+
+
+
 					</div><!--end row-->
 				
 			 

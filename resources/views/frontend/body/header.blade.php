a32a870ce3d832a83d4e4abde23f595908bcb05f
easylearningbd <elebdsoft@gmail.com>
2022-09-05 03:11:13 +0600

    269 Advance Product Search Option Part 3

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -82,7 +82,7 @@ $setting = App\Models\SiteSetting::find(1);
                     <option>Noodles & Rice</option>
                     <option>Ice cream</option>
                 </select>
-                <input name="search" id="search" placeholder="Search for items..." />
+                <input onfocus="search_result_show()" onblur="search_result_hide()" name="search" id="search" placeholder="Search for items..." />
                 <div id="searchProducts"></div>
             </form>
         </div>
@@ -390,8 +390,29 @@ $setting = App\Models\SiteSetting::find(1);
     </header>
 
    <!-- End Header  -->
+<style>
+    #searchProducts{
+        position: absolute;
+        top: 100%;
+        left: 0;
+        width: 100%;
+        background: #ffffff;
+        z-index: 999;
+        border-radius: 8px;
+        margin-top: 5px;
+    }
+</style>
 
+<script>
+    function search_result_show(){
+        $("#searchProducts").slideDown();
 
+    }
+
+    function search_result_hide(){
+        $("#searchProducts").slideUp();
+    }
+</script>
 
 
     <div class="mobile-header-active mobile-header-wrapper-style">
--- resources/views/frontend/product/search_product.blade.php
+++ resources/views/frontend/product/search_product.blade.php
@@ -1,4 +1,51 @@
 
+<style type="text/css">
+	
+	body {
+		 
+	}
+	.card{
+		background-color: #fff;
+		padding: 15px;
+		border: none;
+	}
+	.input-box{
+		position: relative;
+	}
+	.input-box i{
+		position: absolute;
+		right: 13px;
+		top: 15px;
+		color: #ced4da;
+	}
+	.form-control{
+		height: 50px;
+		background-color: #eeeeee69;
+	}
+	.form-control:focus{
+		background-color: #eeeeee69;
+		box-shadow: none;
+		border-color: #eee;
+	}
+	.list{
+		padding-top: 20px;
+		padding-bottom: 10px;
+		display: flex;
+		align-items: center;
+	}
+	.border-bottom{
+		border-bottom: 2px solid #eee;
+	}
+	.list i {
+		font-size: 19px;
+		color: red;
+	}
+	.list small{
+		color: #dedddd;
+	}
+
+
+</style>
 
 @if($products -> isEmpty())
 <h4 class="text-center text-danger">Product Not Found</h4>
@@ -11,11 +58,11 @@
 
 
 		@foreach($products as $item)
-		<a href="">
+		<a href="{{ url('product/details/'.$item->id.'/'.$item->product_slug) }}">
 			<div class="list border-bottom">
 				<img src="{{ asset($item->product_thambnail) }}" style="width:40px; height:40px">
 
-				<div class="d-flex flex-column ml-5">
+				<div class="d-flex flex-column ml-5" style="margin-left:10px; font-size:16px; font-weight: bold;">
  <span>{{ $item->product_name }}</span> <small>${{ $item->selling_price }}</small>
 					
 				</div>
